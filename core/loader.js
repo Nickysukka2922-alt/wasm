2ad4792a02eae6f7b529a3be1ecc1c9bde4fa1d4
Nickysukka2922-alt <nickysukka2922@gmail.com>
2026-05-13 11:43:18 +1000

    Update loader.js

--- core/loader.js
+++ core/loader.js
@@ -1,1 +1,8 @@
-
+var emulator = new V86({
+    wasm_path: "path/to/v86.wasm",
+    screen_container: document.getElementById("screen_container"),
+    bios: { url: "path/to/seabios.bin" },
+    vga_bios: { url: "path/to/vgabios.bin" },
+    cdrom: { url: "path/to/linux.iso" },
+    autostart: true,
+});
